a833da9bd934347129e28372259a37d0e8cca03a
Shubham-Chaprana <shubham.chaprana.ug25@nsut.ac.in>
2026-08-27 16:58:55 +0530

    update allowed hosts

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["ingredia-cyb3.onrender.com"]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
